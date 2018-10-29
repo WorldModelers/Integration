64d11bf30b7c9ae8c49a49769625daa552f67c8b
Brandon Rose <rose.brandon.m@gmail.com>
2018-10-29 16:17:31 -0500

    removed Notebooks from within Docker context and added a `copy Notebooks step` to `start.sh`

--- Docker/UAZ/start.sh
+++ Docker/UAZ/start.sh
@@ -2,10 +2,14 @@
 
 # start.sh
 
+# copy test Notebooks from top-level Notebook directory to Docker Context
+cp -r ../../Notebooks/ .
+
+# set container name variable
 CONTAINER_NAME="indra_box"
 
 # build container
 docker build -f Dockerfile . -t $CONTAINER_NAME
 
-
+# run Docker container
 docker run -it -p 8888:8888 $CONTAINER_NAME /bin/bash
